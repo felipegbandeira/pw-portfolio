6f9c285b71f3684ebc0e0c642e8e3918244b778c
felipegbandeira <felipegbandeira2@gmail.com>
2022-06-18 00:28:03 +0100

    Update

--- portfolio/models.py
+++ portfolio/models.py
@@ -63,4 +63,9 @@ class Tecnologia(models.Model):
     ano = models.IntegerField()
     criador = models.CharField(max_length=50)
 
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.CharField(max_length=500)
+    link = models.CharField(max_length=500)
+    imagem = models.CharField(max_length=500)
 
